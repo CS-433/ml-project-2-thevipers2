818cd8cd4724b9ae86475980238e24c2ca8f91b0
theauv <theau.vannier@epfl.ch>
2021-11-23 22:20:56 +0100

    fonction sample finie

--- preprocessing.py
+++ preprocessing.py
@@ -2,52 +2,85 @@
 """cross validation"""
 import numpy as np
 
-#Pour l'instant marche pour un seul ratio donné et pour un seul data point
+#Globalement fonctionne parfaitement, seul question niveau interpretation plutôt :
+#Le sample est basé sur shuffle, sauf que du coup ça shuffle les temps et les positions, ça change rien pour le NN,
+#mais peut être pour interpretation pas cool
 
-def sample_points(ux, uy, ratios) :
-    
-    #for 1 data point
-    for i, ratio in enumerate(ratios) :
-        new_inds = np.random.uniform(0, ux.shape[0], ratio*ux.shape[0])
-    
+def sample_points(ux, uy, ratio, seed=1) :
+    '''
+    This function samples 1 datapoint which is contained in the matrices ux and uy (matrices obtained by a single simulation).
+    The sampling occurs along the axis 0 which corresponds to the positions of the points on the grid of the simulation.
+    ux : matrix of the velocity along x for different positions (axis 0) and different time steps (axis 1)
+    uy : matrix of the velocity along y for different positions (axis 0) and different time steps (axis 1)
+    ratio : # of positions sampled = ratio * # of positions initially
+    seed : to generate the random for sampling
+    '''
+    # set seed
+    np.random.seed(seed)
+    # generate random indices
+    indices = np.random.permutation(ux.shape[0])
+    new_inds = indices[ : int(np.floor(ratio * ux.shape[0]))]
     new_ux = ux[new_inds, :]
     new_uy = uy[new_inds, :]
     
-    return ux, uy
+    return new_ux, new_uy
 
 
-def sample_times(ux, uy, ratios) :
-        
-    #for 1 data point
-    for i, ratio in enumerate(ratios) :
-        new_inds = np.random.uniform(0, ux.shape[1], ratio*ux.shape[1])
-    
+def sample_times(ux, uy, ratio, seed=1) :
+    '''
+    This function samples 1 datapoint which is contained in the matrices ux and uy (matrices obtained by a single simulation).
+    The sampling occurs along the axis 1 which corresponds to the time steps of the simulation.
+    ux : matrix of the velocity along x for different positions (axis 0) and different time steps (axis 1)
+    uy : matrix of the velocity along y for different positions (axis 0) and different time steps (axis 1)
+    ratios : # of time steps sampled = ratio * # of time steps initially
+    seed : to generate the random for sampling
+    '''
+    # set seed
+    np.random.seed(seed)
+    # generate random indices
+    indices = np.random.permutation(ux.shape[1])
+    new_inds = indices[ : int(np.floor(ratio * ux.shape[1]))]
     new_ux = ux[:, new_inds]
     new_uy = uy[:, new_inds]
     
-    return ux, uy
+    return new_ux, new_uy
 
-#get datapoint n from the matrix Ux or Uy :
-def get_point(U, n) :
-    
-    U_n = U[n*5510:(n+1)*5509, :] #A checker si ça marche
+
+def get_point(U, n, size) :
+    '''
+    Get datapoint number n from the matrix Ux or Uy (recall that 1 datapoint is the output matrix of 1 Stokes simulation)
+    '''
+    U_n = U[n*size:(n+1)*size, :]
     return U_n
 
-def sample(Ux, Uy, ratios_pts, ratios_t) :
-    
+
+def sample(Ux, Uy, ratio_pts, ratio_t, size=5509) : #Checker size !!!!!!!!
+    '''
+    Function that samples the original matrices Ux and Uy along the axis 0 (samples the number of points considered on the grid of 
+    the simulation), and over the axis 1 (samples the number of time steps considered)
+    Ux : Matrix of velocities along x at different position on the grid and at different time step (2D matrix)
+    Uy : Matrix of velocities along y at different position on the grid and at different time step (2D matrix)
+    ratio_pts : # of positions sampled = ratio* # of positions initially
+    ratios_t : # of time_steps sampled = ratio* # of time_steps initially
+    size : # of rows (points) belonging to 1 simulation, in our case 1 simulation = 1 datapoint for our neural networks
+    '''
+    size = int(np.floor(size))
     new_Ux = []
     new_Uy = []
-    for i in range(u.shape[0]/5509) : #A checker
-        ux = get_point(Ux, i)
-        uy = get_point(Uy, i)
-        ux_s1, uy_s1 = sample_points(ux, uy, ratios_pts)
-        ux_s2, uy_s2 = sample_times(ux_s1, uy_s1, ratios_t)
+    for i in range(int(Ux.shape[0]/size)) :
+        ux = get_point(Ux, i, size)
+        uy = get_point(Uy, i, size)
+        print('ux', ux)
+        
+        ux_s1, uy_s1 = sample_points(ux, uy, ratio_pts)
+        ux_s2, uy_s2 = sample_times(ux_s1, uy_s1, ratio_t)
         
         new_Ux.append(ux_s2)
         new_Uy.append(uy_s2)  
-    new_Ux = np.array(new_Ux)
-    new_Uy = np.array(new_Uy)
+        
+    new_Ux = np.concatenate(new_Ux)
+    new_Uy = np.concatenate(new_Uy)
     
-return new_Ux, new_Uy
+    return new_Ux, new_Uy
         
     
