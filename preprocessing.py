158eb8e5c96174d038dae25e9bfb1372b23c3c9b
theauv <theau.vannier@epfl.ch>
2021-11-24 09:53:28 +0100

    ajout get_samples mais marche pas encore

--- preprocessing.py
+++ preprocessing.py
@@ -6,6 +6,8 @@ import numpy as np
 #Le sample est basé sur shuffle, sauf que du coup ça shuffle les temps et les positions, ça change rien pour le NN,
 #mais peut être pour interpretation pas cool
 
+'''Sampling'''
+
 def sample_points(ux, uy, ratio, seed=1) :
     '''
     This function samples 1 datapoint which is contained in the matrices ux and uy (matrices obtained by a single simulation).
@@ -83,4 +85,19 @@ def sample(Ux, Uy, ratio_pts, ratio_t, size=5509) : #Checker size !!!!!!!!
     
     return new_Ux, new_Uy
         
+#ATTENTION : marche pas, nen pas utiliser pour l'instant
+def get_samples(Ux, Uy, ratios_pts, ratios_t, size=5509) :
     
+    ratios_pts.insert(0, 1)
+    ratios_t.insert(0, 1)
+    
+    #Idée : plutot que liste, faire tableau 2D et referer à son voisin direct de gauche si il existe, sinon son voisin du dessus
+    samples_Ux = [Ux]
+    samples_Uy = [Uy]
+    
+    for i in range(1, len(ratios_pts)) :
+        for j in range(1, len(ratios_t)) :
+            new_Ux, new_Uy = sample(samples_Ux, samples_Uy, ratios_pts[i], ratios_t[i], size=samples_Ux.shape[0]*ratio_pts[i-1])
+            samples_Ux.append(new_Ux)
+            samples_Uy.append(new_Uy)
+    return 0 
