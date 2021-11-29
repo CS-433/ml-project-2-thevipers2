8cbfb554a14cd182c23224a462c0adc5f233ef03
Camille Frayssinhes <camille.frayssinhes@epfl.ch>
2021-11-29 18:45:59 +0100

    autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -9,33 +9,25 @@ from torch import nn
 from torch.autograd import Variable 
 
 # define network parameters
-learningRate=1e-3
+learningRate=1e-1
 epochs=100
 
 class Autoencoder(nn.Module) :
-    def __init__(self, inputsize) :
+    def __init__(self) :
         super().__init__()
         
         # encoder network architecture with 4 linear layers
         self.encoder = nn.Sequential(
-        nn.Linear(inputsize, 25),
+        nn.Linear(121000, 12),
         nn.ReLU(True),
-        nn.Linear(25, 12),
-        nn.ReLU(True),
-        nn.Linear(12, 8),
-        nn.ReLU(True),
-        nn.Linear(8, 5)
+        nn.Linear(12, 5),
         )
         
         # decoder network architecture with 4 linear layers
         self.decoder = nn.Sequential(
-        nn.Linear(5, 8),
+        nn.Linear(5, 12),
         nn.ReLU(True),
-        nn.Linear(8, 12),
-        nn.ReLU(True),
-        nn.Linear(12, 25),
-        nn.ReLU(True),
-        nn.Linear(25, inputsize),
+        nn.Linear(12, 121000),
         nn.ReLU(True)
         )
         
@@ -44,22 +36,24 @@ class Autoencoder(nn.Module) :
         x = self.decoder(x)
         return x
     
-    def train(input_data) : 
-         for epoch in range(epochs) : # loop over the dataset multiple times
-            # récupérer les inputs 
-            data = torch.from_numpy(input_data)
-            # optimizer and loss
-            net = Autoencoder(inputsize = (input_data).shape[1])
-            optimizer = torch.optim.Adam(net.parameters(), lr=learningRate, weight_decay=1e-5)
-            criterion = nn.MSELoss()  
+
+net = Autoencoder()
+optimizer = torch.optim.Adam(net.parameters(), lr=learningRate, weight_decay=1e-5)
+criterion = nn.MSELoss()  
+
+def train(input_data) : 
+     for epoch in range(epochs) : # loop over the dataset multiple times
+        # récupérer les inputs 
+        data = torch.from_numpy(input_data)
+        for sim in data : # loop over the data points (simulations) in the dataset 
             # predictions
-            data = data.float()
-            output = net(data)
+            sim = sim.float()
+            output = net(sim)
             # calculate loss
-            loss = criterion(output, data)
+            loss = criterion(output, sim)
             # backpropagation
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            # display the epoch training loss
-            print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, epochs, loss))           
+        # display the epoch training loss
+        print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, epochs, loss))           
