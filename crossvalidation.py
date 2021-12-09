ff1fac8a8ff93c67b477d6790c504fb7956fb63f
theauv <theau.vannier@epfl.ch>
2021-12-09 16:02:43 +0100

    tuning learning rate

--- crossvalidation.py
+++ crossvalidation.py
@@ -15,8 +15,8 @@ from torchvision import datasets, transforms
 from torch.utils.data import Dataset, DataLoader,TensorDataset,random_split,SubsetRandomSampler, ConcatDataset
 from autoencoder import *
 
-
-def Kfold(dataset, k_folds, input_size, epochs, criterion, learningRate, neuron=5, momentum=0.9):
+#momentum note used as optimizer = adam
+def Kfold(dataset, k_folds, input_size, epochs, criterion, learningRate, neuron=5, momentum=0.9, comment = True):
     """
     Perform K-fold cross-validation to estimate the train and test error of the model on the dataset.
 
@@ -45,9 +45,10 @@ def Kfold(dataset, k_folds, input_size, epochs, criterion, learningRate, neuron=
         model = Autoencoder(input_size, neuron)
         optimizer = torch.optim.Adam(model.parameters(), lr=learningRate, weight_decay=1e-5)
         # print
-        print('--------------------------------')
-        print(f'FOLD {fold}')
-        print('--------------------------------')
+        if(comment) :
+            print('--------------------------------')
+            print(f'FOLD {fold}')
+            print('--------------------------------')
         # sample the elements from train_idx and from val_idx and then we convert these samplers into DataLoader objects
         train_sampler = SubsetRandomSampler(train_idx)
         test_sampler = SubsetRandomSampler(val_idx)
@@ -64,8 +65,9 @@ def Kfold(dataset, k_folds, input_size, epochs, criterion, learningRate, neuron=
             train_error = valid_epoch(train_loader,model)
             # compute the relative test error
             test_error=valid_epoch(test_loader,model)
-
-            print("Epoch:{}/{} Training Error:{:.3f} Test Error:{:.3f}".format(epoch + 1,epochs,train_error,test_error))
+            
+            if(comment) :
+                print("Epoch:{}/{} Training Error:{:.3f} Test Error:{:.3f}".format(epoch + 1,epochs,train_error,test_error))
             history['train_error'].append(train_error)
             history['test_error'].append(test_error)
 
@@ -78,26 +80,28 @@ def Kfold(dataset, k_folds, input_size, epochs, criterion, learningRate, neuron=
     for f in range(1,k_folds+1):
         tl_f.append(np.mean(foldperf['fold{}'.format(f)]['train_error']))
         testl_f.append(np.mean(foldperf['fold{}'.format(f)]['test_error']))
-
+    
     print('Performance of {} fold cross validation: '.format(k_folds))
     print("Average Training Error: {:.3f} \t Average Test Error: {:.3f}".format(np.mean(f_train),np.mean(f_test)))
+    print(' ')
     
     diz_ep = {'train_error_ep':[],'test_error_ep':[]}
 
     for i in range(epochs):
           diz_ep['train_error_ep'].append(np.mean([foldperf['fold{}'.format(f+1)]['train_error'][i] for f in range(k_folds)]))
           diz_ep['test_error_ep'].append(np.mean([foldperf['fold{}'.format(f+1)]['test_error'][i] for f in range(k_folds)]))
-
-    # Plot training and test relative errors
-    plt.figure(figsize=(10,8))
-    plt.semilogy(diz_ep['train_error_ep'], label='Train')
-    plt.semilogy(diz_ep['test_error_ep'], label='Test')
-    plt.xlabel('Epoch')
-    plt.ylabel('Error')
-    #plt.grid()
-    plt.legend()
-    plt.title('Autoencoder error')
-    plt.show()
+    
+    if(comment) :
+        # Plot training and test relative errors
+        plt.figure(figsize=(10,8))
+        plt.semilogy(diz_ep['train_error_ep'], label='Train')
+        plt.semilogy(diz_ep['test_error_ep'], label='Test')
+        plt.xlabel('Epoch')
+        plt.ylabel('Error')
+        #plt.grid()
+        plt.legend()
+        plt.title('Autoencoder error')
+        plt.show()
     
     mean_test_err = np.mean(f_test)
     
@@ -105,9 +109,9 @@ def Kfold(dataset, k_folds, input_size, epochs, criterion, learningRate, neuron=
 
 
 
-def Kfold_latent_layer(dataset, k_folds, input_size, epochs, criterion, lr, number_neurons) : 
+def tuning_middle_layer(dataset, k_folds, input_size, epochs, criterion, lr, number_neurons) : 
     """
-    Perform K-fold cross-validation to .
+    Perform K-fold cross-validation to ?????????.
 
     Inputs:
         * dataset (np.array): dataset to perform K-fold cross-validation on 
@@ -120,13 +124,15 @@ def Kfold_latent_layer(dataset, k_folds, input_size, epochs, criterion, lr, numb
     """
     results = []
     for neuron in number_neurons :
-        res = Kfold(dataset, k_folds, input_size, epochs, criterion, lr, neuron)
+        res = Kfold(dataset, k_folds, input_size, epochs, criterion, lr, neuron, comment = False)
         results.append(res)
     best_result = np.min(results)
     best_neuron_number = number_neurons[np.argmin(results)]
     print(f"The results obtained for the number of latent neurons tested are the following : {results}.")
     print(f"The best average test error obtained is {best_result}, and it is obtained with {best_neuron_number} neurons in the latent layer.")
 
+    
+    
 def tuning_lr_momentum(dataset, k_folds, input_size, epochs, criterion, learning_rates, momentums) : #model ??
     """
     Perform K-fold cross-validation to .
@@ -150,15 +156,19 @@ def tuning_lr_momentum(dataset, k_folds, input_size, epochs, criterion, learning
     best_learning_rate = 0
     best_momentum = 0
     for momentum in momentums : 
+        print('\033[1m'+'Momentum = ', str(momentum))
+        print('\033[0m')
         for learning_rate in learning_rates :
-            res = Kfold(dataset, k_folds, input_size, epochs, criterion, learning_rate, momentum)
+            print('\033[1m'+'Learning rate = ', '\033[1m'+str(learning_rate))
+            res = Kfold(dataset, k_folds, input_size, epochs, criterion, learning_rate, momentum, comment = False)
             if(res < best_result) :
                 best_result = res
                 best_learning_rate = learning_rate
                 best_momentum = momentum
+    print('Best learning rate is ', best_learning_rate,' with a best momentum of ', best_momentum, ' with a best error of : ', best_result)
     return best_result, best_learning_rate, best_momentum
 
-def tuning_lr(dataset, k_folds, input_size, epochs, criterion, learning_rates) : #model ??
+def tuning_lr(dataset, k_folds, input_size, epochs, criterion, learning_rates, plot = True) : #model ??
     """
     Perform K-fold cross-validation to .
 
@@ -179,13 +189,27 @@ def tuning_lr(dataset, k_folds, input_size, epochs, criterion, learning_rates) :
     
     best_result = 100000
     best_learning_rate = 0
+    results = []
     for learning_rate in learning_rates :
-        print('learning rate = ', learning_rate)
-        res = Kfold(dataset, k_folds, input_size, epochs, criterion, learning_rate)
+        print('\033[1m'+'Learning rate = ', str(learning_rate))
+        print('\033[0m')
+        res = Kfold(dataset, k_folds, input_size, epochs, criterion, learning_rate, comment = False)
+        results.append(res)
         if(res < best_result) :
             best_result = res
             best_learning_rate = learning_rate
+    print('\033[0m')
     print('Best learning rate is ', best_learning_rate, ' with a best error of : ', best_result)
+    print('\033[0m')
+    
+    if(plot) :
+        plt.plot(learning_rates, results, 'bo')
+        plt.plot(best_learning_rate, best_result, 'ro', markersize=8)
+        plt.xlabel('Learning rate') ; plt.ylabel('Test error')
+        title = 'Average test error on the ' + str(k_folds) + '-fold for different learning rates'
+        plt.title(title)
+        plt.show()
+        
     return best_result, best_learning_rate
 
 
